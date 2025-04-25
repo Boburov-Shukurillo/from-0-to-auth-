d45375a1ccc5c718f6872e28ab92056189f0ef03
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-04-25 07:30:12 +0500

    yangi hayot boshlandi

--- src/shared/dto/register-module.ts
+++
@@ -1,6 +0,0 @@
-export class RegisterDTO {
-    username: string
-    password: string
-    region: string
-    distract: string
-}
--- src/types/user.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-
-export interface User extends Document {
-    username: string, // User name
-    readonly password: string, // User password
-    region: string, // User region
-    district: string, // User district
-}
